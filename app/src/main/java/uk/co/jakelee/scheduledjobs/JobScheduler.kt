7a9d57b5517573acf8fdb77b008df4053b02a778
Jake Lee <github@jakelee.co.uk>
2018-12-02 00:48:07 +0000

    Added buttons, and logging, and... refactoring

---
+++ app/src/main/java/uk/co/jakelee/scheduledjobs/JobScheduler.kt
@@ -0,0 +1,50 @@
+package uk.co.jakelee.scheduledjobs
+
+import android.content.Context
+import android.util.Log
+import com.firebase.jobdispatcher.*
+
+class JobScheduler : JobService() {
+
+    override fun onStartJob(job: JobParameters): Boolean {
+        Log.d("JobScheduler", "Job started")
+        when (job.tag) {
+            SIMPLE_JOB_TAG -> simpleJob(job)
+            else -> return false
+        }
+        return true
+    }
+
+    override fun onStopJob(job: JobParameters?): Boolean {
+        return true
+    }
+
+
+    private fun simpleJob(job: JobParameters) {
+        Log.d("JobScheduler", "Ran job ${job.tag}")
+        jobFinished(job, false)
+    }
+
+    companion object {
+        private const val SIMPLE_JOB_TAG = "uk.co.jakelee.scheduledjobs.job"
+
+        fun scheduleAll(context: Context) {
+            val dispatcher = FirebaseJobDispatcher(GooglePlayDriver(context))
+            val exampleJob = dispatcher.newJobBuilder()
+                .setService(JobScheduler::class.java)
+                .setTag(SIMPLE_JOB_TAG)
+                .setRecurring(true)
+                .setLifetime(Lifetime.FOREVER)
+                .setReplaceCurrent(true)
+                .setRetryStrategy(RetryStrategy.DEFAULT_EXPONENTIAL)
+                .setTrigger(Trigger.executionWindow(5, 10))
+            dispatcher.mustSchedule(exampleJob.build())
+            Log.d("JobScheduler", "Scheduled job")
+        }
+
+        fun cancelAll(context: Context) {
+            FirebaseJobDispatcher(GooglePlayDriver(context)).cancelAll()
+        }
+    }
+
+}
--- app/src/main/java/uk/co/jakelee/scheduledjobs/MainActivity.kt
+++ app/src/main/java/uk/co/jakelee/scheduledjobs/MainActivity.kt
@@ -1,12 +1,15 @@
 package uk.co.jakelee.scheduledjobs
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        scheduleTask.setOnClickListener { JobScheduler.scheduleAll(this) }
+        cancelTask.setOnClickListener { JobScheduler.cancelAll(this) }
     }
 }
